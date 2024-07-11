ecc3bb19b5e9374897a5a9d73c7c45b4b491ee6c
MD Aminul Islam (Rony) <121845719+ronyaminul@users.noreply.github.com>
2024-07-11 22:56:17 +0600

    Add files via upload

---
+++ day-04/assets/js/script.js
@@ -0,0 +1,58 @@
+document.getElementById("main").innerHTML = "Hello JS-Ninja";
+
+//Solution: 01
+const fullName = "MD Aminul islam Rony";
+const email = "ronyislam9044@gmail.com";
+const age = 24;
+const isDeveloper = false;
+const goingToBeDesigner = null;
+
+// Solution: 02
+console.log(`Hey it's me ${fullName}. I am ${age}.you can contact me by this ${email}.I am a Developer and it's ${isDeveloper}. 
+I am going to be a Designer-${goingToBeDesigner}`);
+
+const mydetails =
+  "Hey it's me" +
+  " " +
+  fullName +
+  " . I am " +
+  age +
+  ". you can contact me by this" +
+  " " +
+  email +
+  ".I am a Developer and it's " +
+  isDeveloper +
+  " . I am going to be a Designer" +
+  " " +
+  isGoingToBeDesigner +
+  ".";
+console.log(mydetails);
+
+//Solution: 03
+// let variableName = "BMW"; // let variableName = "BMW"; Error!
+variableName = "Audi";
+console.log(variableName);
+
+// const BrowsersName = "Google Chrome"; Error
+// BrowsersName = "Mozila Firefox";  Error
+const BrowsersName = "Google Chrome";
+console.log(BrowsersName);
+
+// Solution: 04
+let value = "Null";
+console.log(value);
+
+let notValue;
+console.log(notValue);
+
+//Solution: 05
+const notANumber = "tasin" / 4;
+console.log(b);
+
+console.log(typeof b);
+
+//NaN means Not a Number. NaN is a number that is not a legal number.
+
+//Solution: 06
+let car;
+console.log(car);
